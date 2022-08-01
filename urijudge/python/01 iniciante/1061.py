c7e4b3f411c291ffc3b2627fb5aee4b6272c6e39
PainKillerOFC <87381160+PainKillerOFC@users.noreply.github.com>
2022-08-01 19:00:55 -0400

    commit

---
+++ urijudge/python/01 iniciante/1061.py
@@ -0,0 +1,27 @@
+# Como entrada, na primeira linha vai haver a descrição “Dia”,
+# seguido de um espaço e o dia do mês no qual o evento vai começar
+dayi = int(input("Dia "))
+
+# Na linha seguinte, será informado o momento no qual o evento vai iniciar
+# no formato hh : mm : ss
+hi, mi, si = map(int, input().split(" : "))
+
+# Na terceira e quarta linha de entrada haverá outra informação
+# no mesmo formato das duas primeiras linhas, indicando o término do evento.
+
+dayf = int(input("Dia "))
+hf, mf, sf = map(int, input().split(" : "))
+
+
+
+
+
+
+days = 0
+hours = 0
+minutes = 0
+seconds = 0
+print (f"{days}")
+print (f"{hours}")
+print (f"{minutes}")
+print (f"{seconds}")
