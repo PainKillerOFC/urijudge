3f0067e365f1910984fe76622d4da186326894b5
PainKillerOFC <87381160+PainKillerOFC@users.noreply.github.com>
2022-08-02 08:22:44 -0400

    commit

--- urijudge/python/01 iniciante/1061.py
+++ urijudge/python/01 iniciante/1061.py
@@ -1,7 +1,6 @@
 # Como entrada, na primeira linha vai haver a descrição “Dia”,
 # seguido de um espaço e o dia do mês no qual o evento vai começar
-dayi = int(input("Dia "))
-
+di = input("Dia ")
 # Na linha seguinte, será informado o momento no qual o evento vai iniciar
 # no formato hh : mm : ss
 hi, mi, si = map(int, input().split(" : "))
@@ -9,19 +8,31 @@ hi, mi, si = map(int, input().split(" : "))
 # Na terceira e quarta linha de entrada haverá outra informação
 # no mesmo formato das duas primeiras linhas, indicando o término do evento.
 
-dayf = int(input("Dia "))
+df = input("Dia ")
 hf, mf, sf = map(int, input().split(" : "))
 
+# ---------------- CALC ---------------- #
+
+days = df - di
+hours = hf - hi
+minutes = mf - mi
+seconds = sf - si
+
+if df < di:
+    days += 30
+if hf < hi:
+    hours += 24
+    days -= 1
+if mf < mi:
+    minutes += 60
+    hours -= 1
+if sf < si:
+    seconds += 60
+    minutes -= 1
 
 
 
-
-
-days = 0
-hours = 0
-minutes = 0
-seconds = 0
-print (f"{days}")
-print (f"{hours}")
-print (f"{minutes}")
-print (f"{seconds}")
+print (f"{days} dia(s)")
+print (f"{hours} hora(s)")
+print (f"{minutes} minuto(s)")
+print (f"{seconds} segundo(s)")
